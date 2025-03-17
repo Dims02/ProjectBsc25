a3b475d5bb5787c074e936c1f6de0c669efe6fae
Diglette <dimatos.silva@gmail.com>
2025-03-17 14:36:14 +0000

    a

--- views/surveys/editView.php
+++ views/surveys/editView.php
@@ -111,12 +111,12 @@
                 &times;
               </button>
             </div>
-            <textareatiny 
+            <textarea 
               id="question-<?= htmlspecialchars($question->id, ENT_QUOTES, 'UTF-8') ?>" 
               name="questions[<?= htmlspecialchars($question->id, ENT_QUOTES, 'UTF-8') ?>]" 
               placeholder="<?= htmlspecialchars($question->text, ENT_QUOTES, 'UTF-8') ?>" 
               class="w-full p-2 border border-gray-300 rounded mt-2 auto-resize"
-            ><?= htmlspecialchars($question->text, ENT_QUOTES, 'UTF-8') ?></textareatiny>
+            ><?= htmlspecialchars($question->text, ENT_QUOTES, 'UTF-8') ?></textarea>
 
             <!-- Options Container -->
             <div id="mc-options-<?= htmlspecialchars($question->id, ENT_QUOTES, 'UTF-8') ?>" class="ml-4 pt-4">
@@ -327,7 +327,7 @@
 });
 
 tinymce.init({
-      selector: 'textareatiny',  // Use the ID of your textarea or a selector that targets it
+      selector: 'textarea',  // Use the ID of your textarea or a selector that targets it
       plugins: [
         // Core editing features
         'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount', 'autoreresize','autosave',
@@ -336,7 +336,7 @@ tinymce.init({
       tinycomments_mode: 'embedded',
       tinycomments_author: 'Author name',
       autoresize_min_height: 50,
-      height: "300",
+              height: "200",
       mergetags_list: [
         { value: 'First.Name', title: 'First Name' },
         { value: 'Email', title: 'Email' },
