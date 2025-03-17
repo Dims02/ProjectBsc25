77e60b7a00277dfd14d366292307a8024e30ee33
Diglette <dimatos.silva@gmail.com>
2025-03-17 16:57:40 +0000

    a

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,5 +1,5 @@
-<header class=" ">
-      <div class="mx-auto <?= $pos ?> </h1> px-4 py-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?= $heading ?></h1>
-      </div>
+<header>
+  <div class="mx-auto <?= $pos ?> px-4 py-4 sm:px-6 lg:px-8">
+    <h1 class="text-3xl font-bold tracking-tight text-gray-900 sticky"><?= $heading ?></h1>
+  </div>
 </header>
