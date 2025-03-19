9f898e95679a7d443241dff8cab3b2060ddf4af6
Diglette <dimatos.silva@gmail.com>
2025-03-19 22:51:03 +0000

    jj

--- views/headers/surveysView.php
+++ views/headers/surveysView.php
@@ -11,7 +11,7 @@
         <div class="bg-white shadow-md rounded-lg p-6">
           <h3 class="text-lg font-semibold text-gray-900"><?= $survey->title ?></h3>
           <p class="text-gray-600 mt-2"><?= $survey->description ?></p>
-          <a href="survey?id=<?= $survey->id ?>" class="mt-4 inline-block bg-indigo-600 text-white px-4 py-2 rounded-md text-sm hover:bg-indigo-500">
+          <a href="survey?id=<?= $survey->id ?>&page=0" class="mt-4 inline-block bg-indigo-600 text-white px-4 py-2 rounded-md text-sm hover:bg-indigo-500">
             Take Survey
           </a>
         </div>
