046a73fbbcd55dd691e63dfaf800764f0460b436
Diglette <dimatos.silva@gmail.com>
2025-03-20 13:57:34 +0000

    a

--- views/surveys/editView.php
+++ views/surveys/editView.php
@@ -331,22 +331,32 @@
 });
 
 
-  tinymce.init({
-    selector: 'textarea:not(.no-tiny)',
-    plugins: [
-      'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount', 'autoreresize','autosave',
-    ],
-    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-    tinycomments_mode: 'embedded',
-    tinycomments_author: 'Author name',
-    autoresize_min_height: 50,
-    height: "300",
-    mergetags_list: [
-      { value: 'First.Name', title: 'First Name' },
-      { value: 'Email', title: 'Email' },
-    ],
-    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
-  });
+tinymce.init({
+  selector: 'textarea:not(.no-tiny)',
+  setup: function(editor) {
+    editor.on('keydown', function(e) {
+      // 13 is Enter
+      if (e.ctrlKey && e.keyCode === 13) {
+        e.preventDefault();
+        document.getElementById('update-survey-btn').click();
+      }
+    });
+  },
+  plugins: [
+    'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount', 'autoreresize','autosave'
+  ],
+  toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+  tinycomments_mode: 'embedded',
+  tinycomments_author: 'Author name',
+  autoresize_min_height: 50,
+  height: "300",
+  mergetags_list: [
+    { value: 'First.Name', title: 'First Name' },
+    { value: 'Email', title: 'Email' }
+  ],
+  ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant'))
+});
+
 
 
 </script>
