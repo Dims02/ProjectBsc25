79d30ed06171bcedc190ac58754df7ad543b5c19
Diglette <dimatos.silva@gmail.com>
2025-04-30 18:46:33 +0100

    a

--- views/adminView.php
+++ views/adminView.php
@@ -113,7 +113,8 @@
                 <?= htmlspecialchars($user->created_at); ?>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                <?= htmlspecialchars("+" . $user->phone_code . " " . $user->phone); ?>
+                <?php if(isset($user->phone)): ?>
+                  <?=htmlspecialchars("+" . $user->phone_code . " " . $user->phone);  ?>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
               <form method="POST" action="/admin" onsubmit="return confirm('Log in as this user?');" class="inline-block">
